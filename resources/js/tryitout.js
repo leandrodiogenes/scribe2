643c67719c1695e01ad7877e9246c3a819eb6063
Leandro <ldstudio.com@gmail.com>
2020-12-13 15:35:39 -0300

    add content-type header to non formdata requests

--- resources/js/tryitout.js
+++ resources/js/tryitout.js
@@ -1,13 +1,13 @@
-function tryItOut(endpointId) {
+function tryItOut(endpointId){
     document.querySelector(`#btn-tryout-${endpointId}`).hidden = true;
     document.querySelector(`#btn-executetryout-${endpointId}`).hidden = false;
     document.querySelector(`#btn-canceltryout-${endpointId}`).hidden = false;
 
     // Show all input fields
-    document.querySelectorAll(`input[data-endpoint=${endpointId}],label[data-endpoint=${endpointId}]`)
+    document.querySelectorAll(`input[data-endpoint=${endpointId}], label[data-endpoint=${endpointId}]`)
         .forEach(el => el.hidden = false);
 
-    if (document.querySelector(`#form-${endpointId}`).dataset.authed === "1") {
+    if (document.querySelector(`#form-${endpointId}`).dataset.authed === '1') {
         const authElement = document.querySelector(`#auth-${endpointId}`);
         authElement && (authElement.hidden = false);
     }
@@ -16,14 +16,14 @@ function tryItOut(endpointId) {
         .forEach(el => el.open = true);
 }
 
-function cancelTryOut(endpointId) {
+function cancelTryOut(endpointId){
     document.querySelector(`#btn-tryout-${endpointId}`).hidden = false;
     const executeBtn = document.querySelector(`#btn-executetryout-${endpointId}`);
     executeBtn.hidden = true;
-    executeBtn.textContent = "Send Request 💥";
+    executeBtn.textContent = 'Send Request 💥';
     document.querySelector(`#btn-canceltryout-${endpointId}`).hidden = true;
     // hide inputs
-    document.querySelectorAll(`input[data-endpoint=${endpointId}],label[data-endpoint=${endpointId}]`)
+    document.querySelectorAll(`input[data-endpoint=${endpointId}], label[data-endpoint=${endpointId}]`)
         .forEach(el => el.hidden = true);
     document.querySelectorAll(`#form-${endpointId} details`)
         .forEach(el => el.open = false);
@@ -51,31 +51,31 @@ function cancelTryOut(endpointId) {
     }
 }
 
-function makeAPICall(method, path, body, query, headers) {
-    console.log({path, body, query, headers});
+function makeAPICall(method, path, body, query, headers){
+    console.log({ path, body, query, headers });
 
     if (!(body instanceof FormData)) {
-        body = JSON.stringify(body)
+        body = JSON.stringify(body);
     }
 
     const url = new URL(window.baseUrl + '/' + path.replace(/^\//, ''));
 
     // We need this function because if you try to set an array or object directly to a URLSearchParams object,
     // you'll get [object Object] or the array.toString()
-    function addItemToSearchParamsObject(key, value, searchParams) {
-            if (Array.isArray(value)) {
-                value.forEach((v, i) => {
-                    // Append {filters: [first, second]} as filters[0]=first&filters[1]second
-                    addItemToSearchParamsObject(key + '[' + i + ']', v, searchParams);
-                })
-            } else if (typeof value === 'object' && value !== null) {
-                Object.keys(value).forEach((i) => {
-                    // Append {filters: {name: first}} as filters[name]=first
-                    addItemToSearchParamsObject(key + '[' + i + ']', value[i], searchParams);
-                });
-            } else {
-                searchParams.append(key, value);
-            }
+    function addItemToSearchParamsObject(key, value, searchParams){
+        if (Array.isArray(value)) {
+            value.forEach((v, i) => {
+                // Append {filters: [first, second]} as filters[0]=first&filters[1]second
+                addItemToSearchParamsObject(key + '[' + i + ']', v, searchParams);
+            });
+        } else if (typeof value === 'object' && value !== null) {
+            Object.keys(value).forEach((i) => {
+                // Append {filters: {name: first}} as filters[name]=first
+                addItemToSearchParamsObject(key + '[' + i + ']', value[i], searchParams);
+            });
+        } else {
+            searchParams.append(key, value);
+        }
     }
 
     Object.keys(query)
@@ -89,7 +89,7 @@ function makeAPICall(method, path, body, query, headers) {
         .then(response => Promise.all([response.status, response.text(), response.headers]));
 }
 
-function hideCodeSamples(form) {
+function hideCodeSamples(form){
     let codeblock = getPreviousSiblingUntil(form, 'blockquote,pre', 'h2');
     while (codeblock != null) {
         codeblock.style.display = 'none';
@@ -97,12 +97,11 @@ function hideCodeSamples(form) {
     }
 }
 
-function handleResponse(form, endpointId, response, status, headers) {
+function handleResponse(form, endpointId, response, status, headers){
     hideCodeSamples(form);
 
     // Hide error views
     document.querySelector('#execution-error-' + endpointId).hidden = true;
-
 
     const responseContentEl = document.querySelector('#execution-response-content-' + endpointId);
 
@@ -122,34 +121,34 @@ function handleResponse(form, endpointId, response, status, headers) {
     const statusEl = document.querySelector('#execution-response-status-' + endpointId);
     statusEl.textContent = ` (${status})`;
     document.querySelector('#execution-results-' + endpointId).hidden = false;
-    statusEl.scrollIntoView({behavior: "smooth", block: "center"});
+    statusEl.scrollIntoView({ behavior: 'smooth', block: 'center' });
 }
 
-function handleError(form, endpointId, err) {
+function handleError(form, endpointId, err){
     hideCodeSamples(form);
     // Hide response views
     document.querySelector('#execution-results-' + endpointId).hidden = true;
 
     // Show error views
     let errorMessage = err.message || err;
-    errorMessage += "\n\nTip: Check that you're properly connected to the network.";
-    errorMessage += "\nIf you're a maintainer of ths API, verify that your API is running and you've enabled CORS.";
-    errorMessage += "\nYou can check the Dev Tools console for debugging information.";
+    errorMessage += '\n\nTip: Check that you\'re properly connected to the network.';
+    errorMessage += '\nIf you\'re a maintainer of ths API, verify that your API is running and you\'ve enabled CORS.';
+    errorMessage += '\nYou can check the Dev Tools console for debugging information.';
     document.querySelector('#execution-error-message-' + endpointId).textContent = errorMessage;
     const errorEl = document.querySelector('#execution-error-' + endpointId);
     errorEl.hidden = false;
-    errorEl.scrollIntoView({behavior: "smooth", block: "center"});
+    errorEl.scrollIntoView({ behavior: 'smooth', block: 'center' });
 
 }
 
-async function executeTryOut(endpointId, form) {
+async function executeTryOut(endpointId, form){
     const executeBtn = document.querySelector(`#btn-executetryout-${endpointId}`);
-    executeBtn.textContent = "⏱ Sending...";
-    executeBtn.scrollIntoView({behavior: "smooth", block: "center"});
+    executeBtn.textContent = '⏱ Sending...';
+    executeBtn.scrollIntoView({ behavior: 'smooth', block: 'center' });
 
     let body;
     let setter;
-    if (form.dataset.hasfiles === "0") {
+    if (form.dataset.hasfiles === '0') {
         body = {};
         setter = (name, value) => _.set(body, name, value);
     } else {
@@ -165,7 +164,7 @@ async function executeTryOut(endpointId, form) {
         }
 
         if (el.type !== 'radio') {
-            if (value === "" && el.required === false) {
+            if (value === '' && el.required === false) {
                 // Don't include empty optional values in the request
                 return;
             }
@@ -174,7 +173,7 @@ async function executeTryOut(endpointId, form) {
         }
 
         if (el.checked) {
-            value = (value === 'false') ? false : true;
+            value = (value !== 'false');
             setter(el.name, value);
         }
     });
@@ -188,26 +187,29 @@ async function executeTryOut(endpointId, form) {
     urlParameters.forEach(el => (path = path.replace(new RegExp(`\\{${el.name}\\??}`), el.value)));
 
     const headers = JSON.parse(form.dataset.headers);
+    if (form.dataset.hasfiles === '0') {
+        headers["Content-Type"] = "application/json";
+    }
     // Check for auth param that might go in header
-    if (form.dataset.authed === "1") {
+    if (form.dataset.authed === '1') {
         const authHeaderEl = form.querySelector('input[data-component=header]');
         if (authHeaderEl) headers[authHeaderEl.name] = authHeaderEl.dataset.prefix + authHeaderEl.value;
     }
 
     makeAPICall(form.dataset.method, path, body, query, headers)
         .then(([responseStatus, responseContent, responseHeaders]) => {
-            handleResponse(form, endpointId, responseContent, responseStatus, responseHeaders)
+            handleResponse(form, endpointId, responseContent, responseStatus, responseHeaders);
         })
         .catch(err => {
-            console.log("Error while making request: ", err);
+            console.log('Error while making request: ', err);
             handleError(form, endpointId, err);
         })
         .finally(() => {
-            executeBtn.textContent = "Send Request 💥";
+            executeBtn.textContent = 'Send Request 💥';
         });
 }
 
-function getPreviousSiblingUntil(elem, siblingSelector, stopSelector) {
+function getPreviousSiblingUntil(elem, siblingSelector, stopSelector){
     let sibling = elem.previousElementSibling;
     while (sibling) {
         if (sibling.matches(siblingSelector)) return sibling;
